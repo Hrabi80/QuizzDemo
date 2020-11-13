7be03b279e0c4f46d4657054b58fe0b838256fb5
Hrabi <Hrabbi.ahmed88@gmail.com>
2020-11-13 23:05:21 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,15 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { QuizComponent } from './quiz/quiz.component';
 import { HttpClientModule } from '@angular/common/http';
+import { WelcomeDashboardComponent } from './welcome-dashboard/welcome-dashboard.component';
+import { AddQuizComponent } from './add-quiz/add-quiz.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    QuizComponent
+    QuizComponent,
+    WelcomeDashboardComponent,
+    AddQuizComponent
   ],
   imports: [
     BrowserModule,
